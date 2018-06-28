8378a96520d5cb6f9accafb0ae15b1e7dbed6eeb
Paula MacTavish <perilousp@hotmail.com>
2018-06-27 20:11:49 -0400

    Switches npm start

--- src/components/Container/Container.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./Container.css";
-
-const Container = props =>
-  <div className={`container${props.fluid ? "-fluid" : ""}`}>
-    {props.children}
-  </div>;
-
-export default Container;
